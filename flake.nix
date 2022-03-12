16d8f41f17c512fcb0eda7a8b0382516f6cdda5d
hugosenari <hugosenari@gmail.com>
2022-03-12 02:21:31 -0300

    chore: re index finals

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Transparently declare single-set attributes on types.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."finals-master".url = "path:./master";
+  inputs."finals-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."finals-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
