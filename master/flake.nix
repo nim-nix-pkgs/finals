f52696dcc2572144258c637c8e1432c55640416c
hugosenari <hugosenari@gmail.com>
2022-03-22 00:32:27 -0300

    chore: re-index ../nimpkgs/f/finals

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Transparently declare single-set attributes on types.'';
+  description = ''Transparently declare single-set attributes.'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-finals-master.flake = false;
   inputs.src-finals-master.owner = "quelklef";
-  inputs.src-finals-master.ref   = "refs/heads/master";
+  inputs.src-finals-master.ref   = "master";
   inputs.src-finals-master.repo  = "nim-finals";
   inputs.src-finals-master.type  = "github";
   
